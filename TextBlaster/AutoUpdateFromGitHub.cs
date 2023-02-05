385b5eb19f6dc39da05e53e0334138f9d75cc6e0
Eben Zhang <zhangyingneng@gmail.com>
2023-02-05 11:31:30 +0800

    Feat: add build pipeline and auto update for the application

---
+++ TextBlaster/AutoUpdateFromGitHub.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace TextBlaster;
+using Squirrel;
+internal static class AutoUpdateFromGitHub
+{
+    public static async Task<bool> UpdateAsync()
+    {
+        var manager = new GithubUpdateManager("https://github.com/Nicologies/TextBlaster");
+
+        if (Debugger.IsAttached)
+        {
+            // app is trying to upgrade itself.
+            // Debugger.Break();
+            // return false;
+        }
+
+        var updateInfo = await manager.UpdateApp();
+        return updateInfo != null;
+    }
+}
